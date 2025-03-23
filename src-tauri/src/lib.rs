8a4cde2ac713b41cc14171ccffd0607d661ce510
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-23 03:32:30 +0100

    chore: wip

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,16 @@
+use tauri::Manager;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_webview_window("main").unwrap();
+            let _ = main_window.set_ignore_cursor_events(true);
+
+            Ok(())
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
